806b67f8bd0d663b9439842a36f912e59f21dcc7
steveb1250 <60240816+steveb1250@users.noreply.github.com>
2020-01-23 18:38:58 -0500

    Still Needs Work

--- DGEMM/DGEMM.c
+++ DGEMM/DGEMM.c
@@ -3,10 +3,26 @@
 //  DGEMM
 //
 //  Created by Steven Moody on 1/23/20.
-//  Copyright © 2020 Steven Moody. All rights reserved
+//  Copyright © 2020 Steven Moody. All rights reserved.
 //
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
 #include "DGEMM.h"
+
+
+
+void ge(int n, double* A, double* B, double* C){
+    for(int i=0; i< ROWLEN; i++) {
+        for(int j=0; j< ROWLEN; j++) {
+            *(a+i+j*ROWLEN) = (double) rand()/UINT_MAX;
+            *(b+i+j*ROWLEN) = (double) rand()/UINT_MAX;
+            *(c+i+j*ROWLEN) = 0;
+        }
+    }
+}
 
 void mm(double a[ROWLEN][ROWLEN], double b[ROWLEN][ROWLEN], double c[ROWLEN][ROWLEN]) {
     
@@ -20,18 +36,24 @@ void mm(double a[ROWLEN][ROWLEN], double b[ROWLEN][ROWLEN], double c[ROWLEN][ROW
     }
 }
 
-void dgemm (int n, double* A, double* B, double* C)
-{
+void dgemm (int n, double* A, double* B, double* C){
+    
     for (int i = 0; i < n; ++i)
-        for (int j = 0; j < n; ++j)
-        {
+        for (int j = 0; j < n; ++j) {
             double cij = C[i+j*n]; /* cij = C[i][j] */
-            for( int k = 0; k < n; k++ )
-                cij += A[i+k*n] * B[k+j*n]; /* cij += A[i][k]*B[k][j] */
-            C[i+j*n] = cij; /* C[i][j] = cij */
-        }
+                for( int k = 0; k < n; k++ )
+                    cij += A[i+k*n] * B[k+j*n]; /* cij += A[i][k]*B[k][j] */
+                    C[i+j*n] = cij; /* C[i][j] = cij */
+    }
 }
 
+void output(int n, double* A, double* B, double* C){
+for (int i = 0; i <  10; i++) {
+    for (int j = 0; j < 1; j++) {
+        printf("c[%d][%d] = %lf\n", i,j, c[i+j*ROWLEN] );
+    }
+  }
+}
 
 void dgemm_intrins() {
 }
