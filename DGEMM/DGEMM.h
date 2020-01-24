b2f155457dc6346da1a3515c903be4d48e3ff735
steveb1250 <60240816+steveb1250@users.noreply.github.com>
2020-01-23 19:22:36 -0500

    Update DGEMM.h

--- DGEMM/DGEMM.h
+++ DGEMM/DGEMM.h
@@ -5,20 +5,25 @@
 //  Created by Steven Moody on 1/23/20.
 //  Copyright © 2020 Steven Moody. All rights reserved.
 //
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
 #ifndef DGEMM_h
 #define DGEMM_h
 
 #define ROWLEN 1024
 #define MSIZE [ROWLEN][ROWLEN]
-
-    void ge(int n, double* A, double* B, double* C);
+double* a;
+double* b;
+double* c;
+    void ge(int n, double* a, double* b, double* c);
 
     void mm(double a[ROWLEN][ROWLEN], double b[ROWLEN][ROWLEN], double c[ROWLEN][ROWLEN]);
     
-    void dgemm (int n, double* A, double* B, double* C);
+    void dgemm (int n, double* a, double* b, double* c);
 
-    void output(int n, double* A, double* B, double* C);
+    void output(int n, double* a, double* b, double* c);
 
 
 #endif /* DGEMM_h */
